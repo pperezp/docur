c8ccf8d95a34e26450ebf3e5139c007b6d109c07
pperezp <patodeath@gmail.com>
2018-05-15 20:54:03 -0400

    Se eliminan las tablas de docentes y cursos en html

--- view/crearCurso.php
+++ view/crearCurso.php
@@ -7,30 +7,20 @@
     <body>
         <h1>Crear Curso</h1>
         <form action="../controller/crearCurso.php" method="post">
-            <input type="text" name="nombre" placeholder="Nombre:" required>
+            <input type="text" name="nombre" placeholder="Nombre:" required list="cursos">
             <input type="submit" value="Registrar Curso">
         </form>
         
         <a href="../index.php">Volver</a>
         
-        <h2>Listado de cursos</h2>
-        <table border="1">
-            <tr>
-                <th>ID</th>
-                <th>Nombre</th>
-            </tr>
-            
-            <?php
-            require_once '../model/Data.php';
-            $d = new Data();
-            
-            foreach($d->getAllCursos() as $cur){
-                echo "<tr>";
-                    echo "<td>$cur->id</td>";
-                    echo "<td>$cur->nombre</td>";
-                echo "</tr>";
-            }
-            ?>
-        </table>
+        <datalist id="cursos">
+        <?php
+        require_once '../model/Data.php';
+        $d = new Data();
+        foreach ($d->getAllCursos() as $c){
+            echo "<option value='$c->nombre'>";
+        }
+        ?>
+        </datalist>
     </body>
 </html>
--- view/crearDocente.php
+++ view/crearDocente.php
@@ -7,33 +7,37 @@
     <body>
         <h1>Crear Docente</h1>
         <form action="../controller/crearDocente.php" method="post">
-            <input type="text" name="rut" placeholder="Rut:" required>
-            <input type="text" name="nombre" placeholder="Nombre:" required>
+            <input type="text" name="rut" placeholder="Rut:" required list="ruts">
+            <input type="text" name="nombre" placeholder="Nombre:" required list="nombres">
             <input type="submit" value="Registrar docente">
         </form>
-        
+
         <a href="../index.php">Volver</a>
-        
-        <h2>Listado de docentes</h2>
-        <table border="1">
-            <tr>
-                <th>ID</th>
-                <th>Rut</th>
-                <th>Nombre</th>
-            </tr>
-            
-            <?php
-            require_once '../model/Data.php';
-            $d = new Data();
-            
-            foreach($d->getAllDocentes() as $doc){
-                echo "<tr>";
-                    echo "<td>$doc->id</td>";
-                    echo "<td>$doc->rut</td>";
-                    echo "<td>$doc->nombre</td>";
-                echo "</tr>";
-            }
-            ?>
-        </table>
+
+        <?php
+        require_once '../model/Data.php';
+        $lista = array();
+        $d = new Data();
+        foreach ($d->getAllDocentes() as $doc) {
+            array_push($lista, $doc);
+        }
+        ?>
+
+        <datalist id="nombres">
+        <?php
+        foreach ($lista as $doc){
+            echo "<option value='$doc->nombre'>";
+        }
+        ?>
+        </datalist>
+
+        <datalist id="ruts">
+        <?php
+        foreach ($lista as $doc){
+            echo "<option value='$doc->rut'>";
+        }
+        ?>
+        </datalist>
+
     </body>
 </html>
